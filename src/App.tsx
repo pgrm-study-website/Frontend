1c225fa929b8d2b2d9faf7b2a968f6a24daaa812
Seungha Jeon <seuha516@naver.com>
2022-03-21 00:03:32 +0900

    [Feat] Section 요소들 위치 설정
    Header, Footer, Sidebar의 틀만 잡아두었습니다.

--- src/App.tsx
+++ src/App.tsx
@@ -13,6 +13,9 @@ import NanumSquareR from 'assets/fonts/NanumSquareR.ttf';
 import Cafe24SsurroundAir from 'assets/fonts/Cafe24SsurroundAir.ttf';
 
 import Login from 'pages/Login';
+import Sidebar from 'components/sidebar/Sidebar';
+import Header from 'components/header/Header';
+import Footer from 'components/footer/Footer';
 
 const GlobalStyles = createGlobalStyle`
   ${reset}
@@ -118,11 +121,14 @@ const App = () => {
             path="/*"
             element={
               <Wrapper>
+                <Sidebar />
+                <Header />
                 <Routes>
                   <Route path="" element={<div>Main</div>} />
                   <Route path="write" element={<div>Write</div>} />
                   <Route path="*" element={<div>Not Found</div>} />
                 </Routes>
+                <Footer />
               </Wrapper>
             }
           />
