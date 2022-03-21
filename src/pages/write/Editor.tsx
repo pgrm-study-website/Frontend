002027feb2d7a8016aa8874c08b08049a3795dfd
Seungha Jeon <seuha516@naver.com>
2022-03-22 01:37:01 +0900

    [Revert] Write 페이지 일부분 구현
    This reverts commit 7d95ba20efd9dd39c76f8e201e59ba9847fa7ce3.

--- src/pages/write/Editor.tsx
+++ src/pages/write/Editor.tsx
@@ -3,6 +3,15 @@ import styled from 'styled-components';
 import ReactQuill from 'react-quill';
 import 'react-quill/dist/quill.snow.css';
 
+const QuillWrapper = styled.div`
+  background-color: white;
+  .ql-editor {
+    padding: 20px 15px 100px 15px;
+    min-height: 420px;
+    max-height: 550px;
+  }
+`;
+
 const Editor = () => {
   const QuillRef = useRef<ReactQuill>();
   const modules = {
@@ -35,42 +44,19 @@ const Editor = () => {
   ];
 
   return (
-    <Wrapper>
-      <NameText>본문</NameText>
-      <QuillWrapper>
-        <ReactQuill
-          ref={element => {
-            if (element !== null) {
-              QuillRef.current = element;
-            }
-          }}
-          modules={modules}
-          formats={formats}
-          theme="snow"
-        />
-      </QuillWrapper>
-    </Wrapper>
+    <QuillWrapper>
+      <ReactQuill
+        ref={element => {
+          if (element !== null) {
+            QuillRef.current = element;
+          }
+        }}
+        modules={modules}
+        formats={formats}
+        theme="snow"
+      />
+    </QuillWrapper>
   );
 };
 
 export default Editor;
-
-const Wrapper = styled.div`
-  width: 100%;
-  display: flex;
-  flex-direction: column;
-  margin-top: 30px;
-`;
-const NameText = styled.div`
-  font-size: 30px;
-  font-family: LeeSeoyun;
-  margin-bottom: 10px;
-`;
-const QuillWrapper = styled.div`
-  background-color: white;
-  .ql-editor {
-    padding: 20px 15px 100px 15px;
-    min-height: 420px;
-    max-height: 550px;
-  }
-`;
--- src/pages/write/Write.tsx
+++ src/pages/write/Write.tsx
@@ -1,27 +1,19 @@
 import React from 'react';
 import styled from 'styled-components';
-import Banner from './Banner';
-import Category from './Category';
 import Editor from './Editor';
-import Tags from './Tags';
-import Title from './Title';
+
+const Wrapper = styled.div`
+  background-color: #d6d6d6;
+  width: 100%;
+  max-width: 1000px;
+`;
 
 const Write = () => {
   return (
     <Wrapper>
-      <Banner />
-      <Title />
-      <Category />
       <Editor />
-      <Tags />
     </Wrapper>
   );
 };
 
 export default Write;
-
-const Wrapper = styled.div`
-  width: 100%;
-  max-width: 1000px;
-  padding: 50px 20px;
-`;
