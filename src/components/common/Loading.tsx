a29d287477e07dc5706cc9e78cdf3ab589417a01
Seungha Jeon <seuha516@naver.com>
2022-03-22 22:44:17 +0900

    [Feat] Loading 완성
    간단하게 만들었습니다

--- src/components/common/Loading.tsx
+++ src/components/common/Loading.tsx
@@ -1,7 +1,77 @@
 import React from 'react';
+import styled from 'styled-components';
 
 const Loading = () => {
-  return <div>Loading</div>;
+  return (
+    <Wrapper>
+      <PlmingText>Plming</PlmingText>
+      <LoadingComponent r="80px" />
+    </Wrapper>
+  );
 };
 
 export default Loading;
+
+export const LoadingBox = ({ r }: { r: string }) => {
+  return (
+    <FlexBox>
+      <LoadingComponent r={r} />
+    </FlexBox>
+  );
+};
+export const LoadingComponent = ({ r }: { r: string }) => {
+  return <LoadingItem style={{ width: r, height: r }} />;
+};
+
+const Wrapper = styled.div`
+  width: 100%;
+  min-height: calc(100vh - 100px);
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+const PlmingText = styled.div`
+  font-size: 80px;
+  font-family: 'Hurricane', cursive;
+  animation: opacityChange 2s infinite linear;
+  @keyframes opacityChange {
+    0% {
+      opacity: 0.8;
+    }
+    50% {
+      opacity: 0.4;
+    }
+    100% {
+      opacity: 0.8;
+    }
+  }
+  margin-bottom: 20px;
+`;
+const FlexBox = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+const LoadingItem = styled.div`
+  border: 3px solid #565656;
+  border-top-color: white;
+  border-right-color: white;
+  border-radius: 100%;
+  -ms-user-select: none;
+  -moz-user-select: -moz-none;
+  -webkit-user-select: none;
+  -khtml-user-select: none;
+  user-select: none;
+  animation: spin 1s infinite linear;
+  @keyframes spin {
+    from {
+      transform: rotate(0deg);
+    }
+    to {
+      transform: rotate(359deg);
+    }
+  }
+`;
