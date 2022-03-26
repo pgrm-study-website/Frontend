93d656ad1ea9de843767bafcc82b2394dfb399a3
sumi.byun <selina2000@naver.com>
2022-03-26 19:09:35 +0900

    Feat : SideBar 구현

--- src/components/sections/sidebar/Sidebar.tsx
+++ src/components/sections/sidebar/Sidebar.tsx
@@ -1,11 +1,86 @@
 import React from 'react';
 import styled from 'styled-components';
-
+import img from 'assets/images/profile.png';
+import { BsPersonCircle } from 'react-icons/bs';
+import { AiOutlineLogout } from 'react-icons/ai';
+import { IoIosNotifications } from 'react-icons/io';
+import { AiOutlineMessage } from 'react-icons/ai';
 const Sidebar = () => {
-  return <Wrapper>Sidebar</Wrapper>;
+  return (
+    <Wrapper>
+      <Title>플밍</Title>
+      <NotificationContainer>
+        <IoIosNotifications />
+      </NotificationContainer>
+      <Profile>
+        <img src={img} alt="profile" />
+        <Name>seuha516</Name>
+      </Profile>
+      <LinkContainer>
+        <LinkItem>
+          <LinkIcon>
+            <BsPersonCircle />
+          </LinkIcon>
+          <LinkText>mypage</LinkText>
+        </LinkItem>
+        <LinkItem>
+          <LinkIcon>
+            <AiOutlineMessage />
+          </LinkIcon>
+          <LinkText>message</LinkText>
+        </LinkItem>
+        <LinkItem>
+          <LinkIcon>
+            <AiOutlineLogout />
+          </LinkIcon>
+          <LinkText>logout</LinkText>
+        </LinkItem>
+      </LinkContainer>
+    </Wrapper>
+  );
 };
 
 export default Sidebar;
+const NotificationContainer = styled.div`
+  position: absolute;
+  font-size: 20px;
+  color: #242424;
+  right: 20px;
+`;
+const Title = styled.div`
+  font-size: 50px;
+  color: #fff;
+`;
+const Profile = styled.div`
+  color: #242424;
+  font-weight: 700;
+`;
+const LinkContainer = styled.div`
+  display: flex;
+  gap: 10px;
+  flex-direction: column;
+  color: #242424;
+`;
+const LinkItem = styled.div`
+  display: flex;
+  height: 30px;
+  font-size: 20px;
+  align-items: center;
+  justify-content: center;
+`;
+const LinkText = styled.div`
+  width: 100px;
+  text-align: left;
+`;
+const LinkIcon = styled.div`
+  width: 40px;
+  position: relative;
+  top: 4px;
+`;
+const Name = styled.div`
+  font-size: 20px;
+  padding: 20px;
+`;
 
 const Wrapper = styled.div`
   background-color: #4cbbc2;
@@ -16,6 +91,9 @@ const Wrapper = styled.div`
   left: max(0px, calc(50% - 750px));
   display: flex;
   flex-direction: column;
+  text-align: center;
+  gap: 20px;
+  padding: 20px;
   @media all and (max-width: 1510px) {
     width: 215px;
   }
