ca67f5331198252a461e98fca9a84eba11e9ac69
Seungha Jeon <seuha516@naver.com>
2022-04-26 17:15:32 +0900

    [Fix] 메시지 정리

--- src/modules/message.ts
+++ src/modules/message.ts
@@ -1,9 +1,10 @@
 import { createAction, ActionType, createReducer } from 'typesafe-actions';
 import { AxiosError } from 'axios';
-import { call, takeLatest } from 'redux-saga/effects';
+import { takeLatest } from 'redux-saga/effects';
 import createRequestSaga from 'lib/createRequestSaga';
 import * as messageAPI from 'lib/api/message';
 
+const CHANGE_FIELD = 'notices/CHANGE_FIELD';
 const MESSAGE_READ = 'notices/MESSAGE_READ';
 const MESSAGE_READ_SUCCESS = 'notices/MESSAGE_READ_SUCCESS';
 const MESSAGE_READ_FAILURE = 'notices/MESSAGE_READ_FAILURE';
@@ -13,12 +14,15 @@ const MESSAGE_DETAIL_READ_FAILURE = 'notices/MESSAGE_DETAIL_READ_FAILURE';
 const MESSAGE_DELETE_ALL = 'notices/MESSAGE_DELETE_ALL';
 const MESSAGE_DELETE_ALL_SUCCESS = 'notices/MESSAGE_DELETE_ALL_SUCCESS';
 const MESSAGE_DELETE_ALL_FAILURE = 'notices/MESSAGE_DELETE_ALL_FAILURE';
-const MESSAGE_DELETE_ONE = 'notices/MESSAGE_DELETE_ONE';
-const MESSAGE_DELETE_ONE_SUCCESS = 'notices/MESSAGE_DELETE_ONE_SUCCESS';
-const MESSAGE_DELETE_ONE_FAILURE = 'notices/MESSAGE_DELETE_ONE_FAILURE';
+// const MESSAGE_DELETE_ONE = 'notices/MESSAGE_DELETE_ONE';
+// const MESSAGE_DELETE_ONE_SUCCESS = 'notices/MESSAGE_DELETE_ONE_SUCCESS';
+// const MESSAGE_DELETE_ONE_FAILURE = 'notices/MESSAGE_DELETE_ONE_FAILURE';
 const MESSAGE_SEND = 'notices/MESSAGE_SEND';
 const MESSAGE_SEND_SUCCESS = 'notices/MESSAGE_SEND_SUCCESS';
 const MESSAGE_SEND_FAILURE = 'notices/MESSAGE_SEND_FAILURE';
+
+export const changeField =
+  createAction(CHANGE_FIELD)<{ key: string; value: any }>();
 
 export const messageRead =
   createAction(MESSAGE_READ)<messageAPI.readMessages>();
@@ -42,13 +46,14 @@ export const messageDeleteAllFailure = createAction(
   MESSAGE_DELETE_ALL_FAILURE,
 )<AxiosError>();
 
-export const messageDeleteOne = createAction(MESSAGE_DELETE_ONE)();
-export const messageDeleteOneSuccess = createAction(
-  MESSAGE_DELETE_ONE_SUCCESS,
-)<any>();
-export const messageDeleteOneFailure = createAction(
-  MESSAGE_DELETE_ONE_FAILURE,
-)<AxiosError>();
+// export const messageDeleteOne = createAction(MESSAGE_DELETE_ONE)();
+// export const messageDeleteOneSuccess = createAction(
+//   MESSAGE_DELETE_ONE_SUCCESS,
+// )<any>();
+// export const messageDeleteOneFailure = createAction(
+//   MESSAGE_DELETE_ONE_FAILURE,
+// )<AxiosError>();
+
 export const messageSend = createAction(MESSAGE_SEND)<any>();
 export const messageSendSuccess = createAction(MESSAGE_SEND_SUCCESS)<any>();
 export const messageSendFailure =
@@ -65,21 +70,22 @@ const removeAllSaga = createRequestSaga(
   MESSAGE_DELETE_ALL,
   messageAPI.removeAll,
 );
-const removeOneSaga = createRequestSaga(
-  MESSAGE_DELETE_ONE,
-  messageAPI.removeOne,
-);
+// const removeOneSaga = createRequestSaga(
+//   MESSAGE_DELETE_ONE,
+//   messageAPI.removeOne,
+// );
 const sendSaga = createRequestSaga(MESSAGE_SEND, messageAPI.send);
 
 export function* messagesSaga() {
   yield takeLatest(MESSAGE_READ, readSaga);
   yield takeLatest(MESSAGE_DETAIL_READ, readDetailSaga);
   yield takeLatest(MESSAGE_DELETE_ALL, removeAllSaga);
-  yield takeLatest(MESSAGE_DELETE_ONE, removeOneSaga);
+  // yield takeLatest(MESSAGE_DELETE_ONE, removeOneSaga);
   yield takeLatest(MESSAGE_SEND, sendSaga);
 }
 
 const actions = {
+  changeField,
   messageRead,
   messageReadSuccess,
   messageReadFailure,
@@ -89,9 +95,9 @@ const actions = {
   messageDeleteAll,
   messageDeleteAllSuccess,
   messageDeleteAllFailure,
-  messageDeleteOne,
-  messageDeleteOneSuccess,
-  messageDeleteOneFailure,
+  // messageDeleteOne,
+  // messageDeleteOneSuccess,
+  // messageDeleteOneFailure,
   messageSend,
   messageSendSuccess,
   messageSendFailure,
@@ -100,37 +106,33 @@ type messagesAction = ActionType<typeof actions>;
 
 export type messageState = {
   messages: Array<messageAPI.messagesProps> | null;
-  remove: boolean | null;
   error: AxiosError | null;
 };
 export type messageDetailState = {
   messageDetail: Array<messageAPI.messagesDetailProps> | null;
-  sendMessage: messageAPI.messagesDetailProps | null;
-  remove: boolean | null;
+  reload: boolean | null;
   error: AxiosError | null;
 };
 const initialState: messageState = {
   messages: null,
-  remove: null,
   error: null,
 };
 const initialMessageDetailState: messageDetailState = {
   messageDetail: null,
-  sendMessage: null,
-  remove: null,
+  reload: null,
   error: null,
 };
 const messages = createReducer<messageState, messagesAction>(initialState, {
   [MESSAGE_READ]: state => ({
     ...state,
-    messages: null,
+    // 초기화 안함 (화면 깜빡임 방지)
   }),
   [MESSAGE_READ_SUCCESS]: (state: any, { payload }: any) => ({
     ...state,
     messages: payload.data,
   }),
   [MESSAGE_READ_FAILURE]: (state: any, { payload: error }: any) => {
-    alert('message reading error');
+    alert(error.response?.data.message);
     return {
       ...state,
       error,
@@ -138,64 +140,65 @@ const messages = createReducer<messageState, messagesAction>(initialState, {
   },
 });
 export const messageDetail = createReducer(initialMessageDetailState, {
-  [MESSAGE_DETAIL_READ]: (state, { payload }) => ({
+  [CHANGE_FIELD]: (state, { payload: { key, value } }) => ({
     ...state,
-    messageDetail: null,
+    [key]: value,
+  }),
+  [MESSAGE_DETAIL_READ]: state => ({
+    ...state,
+    // 초기화 안함 (화면 깜빡임 방지)
   }),
   [MESSAGE_DETAIL_READ_SUCCESS]: (state, { payload }) => ({
     ...state,
     messageDetail: payload.data,
   }),
   [MESSAGE_DETAIL_READ_FAILURE]: (state, { payload: error }) => {
-    // alert('message reading error');
     alert(error.response?.data.message);
     return {
       ...state,
       error,
     };
   },
-  [MESSAGE_DELETE_ALL]: (state, { payload }) => ({
+  [MESSAGE_DELETE_ALL]: state => ({
     ...state,
-    messageDetail: null,
+    reload: null,
   }),
-  [MESSAGE_DELETE_ALL_SUCCESS]: (state, { payload }) => ({
+  [MESSAGE_DELETE_ALL_SUCCESS]: state => ({
     ...state,
-    remove: true,
-    messageDetail: payload.data,
+    reload: true,
   }),
   [MESSAGE_DELETE_ALL_FAILURE]: (state, { payload: error }) => {
-    alert('message delete error');
+    alert(error.response?.data.message);
     return {
       ...state,
       error,
     };
   },
-  [MESSAGE_DELETE_ONE]: (state, { payload }) => ({
+  // [MESSAGE_DELETE_ONE]: state => ({
+  //   ...state,
+  //   messageDetail: null,
+  // }),
+  // [MESSAGE_DELETE_ONE_SUCCESS]: (state, { payload }) => ({
+  //   ...state,
+  //   remove: true,
+  //   messageDetail: payload.data,
+  // }),
+  // [MESSAGE_DELETE_ONE_FAILURE]: (state, { payload: error }) => {
+  //   alert(error.response?.data.message);
+  //   return {
+  //     ...state,
+  //     error,
+  //   };
+  // },
+  [MESSAGE_SEND]: state => ({
     ...state,
-    messageDetail: null,
+    reload: null,
   }),
-  [MESSAGE_DELETE_ONE_SUCCESS]: (state, { payload }) => ({
+  [MESSAGE_SEND_SUCCESS]: state => ({
     ...state,
-    remove: true,
-    messageDetail: payload.data,
-  }),
-  [MESSAGE_DELETE_ONE_FAILURE]: (state, { payload: error }) => {
-    alert('message delete error');
-    return {
-      ...state,
-      error,
-    };
-  },
-  [MESSAGE_SEND]: (state, { payload }) => ({
-    ...state,
-    messageDetail: null,
-  }),
-  [MESSAGE_SEND_SUCCESS]: (state, { payload }) => ({
-    ...state,
-    sendMessage: payload.data,
+    reload: true,
   }),
   [MESSAGE_SEND_FAILURE]: (state, { payload: error }) => {
-    alert('message send error');
     alert(error.response?.data.message);
     return {
       ...state,
@@ -203,4 +206,5 @@ export const messageDetail = createReducer(initialMessageDetailState, {
     };
   },
 });
+
 export default messages;
